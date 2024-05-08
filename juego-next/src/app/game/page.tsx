bbe61a02d2d44605c6cadc8f54bae8a1486a20fb
Adri-SanMa <sanmartinadriel@gmail.com>
2024-05-08 08:28:49 -0300

    Cambio de ubicacion de imagenes, de referencia en page y creacion de color verde custom

--- juego-next/src/app/game/page.tsx
+++ juego-next/src/app/game/page.tsx
@@ -2,14 +2,15 @@
 
 import React from 'react';
 
-function game() {
-  return (
-    <div style={{ backgroundColor: 'green', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div style={{ position: 'relative' }}>
-        <img src="/images/WE - House.png" alt="House" style={{ maxWidth: '100%', maxHeight: '100%', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />
+function Game() {
+    return (
+      <div className="bg-grassGreen min-h-screen flex items-center justify-center">
+        <div className="relative h-64 w-64">
+            <img src="/images/WE - House.png" alt="House" className="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
-export default game;
+export default Game;
+
--- juego-next/tailwind.config.ts
+++ juego-next/tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      grassGreen: '#41980a',
+    }
   },
   plugins: [],
 };
