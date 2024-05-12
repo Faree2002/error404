641a625a1197688c62d13f6614d1b0abe727ce76
Faree2002 <162990815+Faree2002@users.noreply.github.com>
2024-05-12 18:38:57 -0300

    feat: Add lazy loading for game page images and implement building classes
    The commit adds lazy loading functionality for the images on the game page, improving page load performance. Additionally, new building classes for AlmacenMadera and AlmacenPiedra are implemented, which inherit from the base Building class and define their own attributes and methods.
    
    Note: Please remove any unnecessary details and only include the commit message in the markdown code block.

--- juego-next/src/app/game/page.tsx
+++ juego-next/src/app/game/page.tsx
@@ -1,16 +1,53 @@
-// pages/game.tsx
+'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function Game() {
-    return (
-      <div className="bg-grassGreen min-h-screen flex items-center justify-center">
-        <div className="relative h-64 w-64">
-            <img src="/images/WE - House.png" alt="House" className="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
-        </div>
+  const [imageVisible, setImageVisible] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState({ x: 0, y: 0 });
+
+  const handleButtonClick = () => {
+    setImageVisible(true);
+  };
+
+  const handleImageClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    setSelectedPosition({ x, y });
+  };
+
+  return (
+    <div className="bg-grassGreen min-h-screen flex items-center justify-center">
+      <div className="relative h-64 w-64">
+        {imageVisible && (
+          <img
+            src="/images/WE - House.png"
+            alt="House"
+            className="w-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={handleImageClick}
+          />
+        )}
       </div>
-    );
+      <div className="absolute bottom-4 left-4">
+        <button
+          className="bg-amber-400 hover:bg-amber-300 text-white font-bold drop-shadow-[0_1.2px_1.5px_rgba(0,0,0,0.8)] py-2 px-4 border border-amber-400 rounded-full"
+          style={{ textShadow: '0.5px 0.5px black, -0.5px -0.5px black, 0.5px -0.5px black, -0.5px 0.5px black' }}
+          onClick={handleButtonClick}
+        >
+          Tienda
+        </button>
+      </div>
+      {selectedPosition && (
+        <div
+          className="absolute"
+          style={{ left: selectedPosition.x, top: selectedPosition.y }}
+        >
+          <img src="/images/WE - House.png" alt="House" className="w-1/2" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Game;
-
