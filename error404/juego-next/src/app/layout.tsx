c1600b0d88a31c1f8db9b8df43e6649c4aa65b25
Alenicoburger <naprosman@gmail.com>
2024-05-21 14:10:12 -0300

    nico

--- error404/juego-next/src/app/layout.tsx
+++ error404/juego-next/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
