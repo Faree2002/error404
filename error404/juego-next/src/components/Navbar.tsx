0c803b53d67c69db138373d7c62313627573f9d6
Faree2002 <162990815+Faree2002@users.noreply.github.com>
2024-06-10 08:32:42 -0300

    chore: Add link to homepage in Navbar component

--- error404/juego-next/src/components/Navbar.tsx
+++ error404/juego-next/src/components/Navbar.tsx
@@ -10,7 +10,11 @@ async function Navbar() {
     <nav className="flex justify-between items-center bg-gray-950 text-white px-24 py-3">
       <h1 className="flex items-center text-xl font-bold">
         <img className="w-6 h-6 mr-2" src="favicon.ico" alt="Logo" />
+        <Link href="/" passHref>
+        <button className="bg-transparent">
         Game of Truenos
+        </button>
+        </Link>
       </h1>
 
       <ul className="flex gap-x-2">
