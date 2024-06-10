6779f5ca9daaf0df8dd6744790262164b7517c40
Faree2002 <162990815+Faree2002@users.noreply.github.com>
2024-06-09 22:51:15 -0300

    chore: Update Navbar component to include logo image

--- error404/juego-next/src/components/Navbar.tsx
+++ error404/juego-next/src/components/Navbar.tsx
@@ -8,7 +8,10 @@ async function Navbar() {
 
   return (
     <nav className="flex justify-between items-center bg-gray-950 text-white px-24 py-3">
-      <h1 className="text-xl font-bold">Game of Truenos</h1>
+      <h1 className="flex items-center text-xl font-bold">
+        <img className="w-6 h-6 mr-2" src="favicon.ico" alt="Logo" />
+        Game of Truenos
+      </h1>
 
       <ul className="flex gap-x-2">
         {!session?.user ? (
